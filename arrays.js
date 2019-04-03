b9a99ab5ef6bd85093feb1ac956fdfda227dcde5
Ezra Brand <ezrabrand@gmail.com>
2019-04-03 17:08:33 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,13 @@
 chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittle']
+
+addElementToBeginningOfArray(x) {
+  let myArray = []
+  [...x]
+  return myArray
+}
+
+destructivelyAddElementToBeginningOfArray(x) {
+  let myArray = []
+  myArray.unshift(x)
+  return myArray
+}
