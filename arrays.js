9766aa123b7a0e704c529f2e6f748049379dbc89
Ezra Brand <ezrabrand@gmail.com>
2019-04-03 17:49:28 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -16,12 +16,15 @@ function accessElementInArray(array,index) {
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
   array = array.shift()
+  return array
 }
 
 function removeElementFromBeginningOfArray(array) {
   array.slice(0,1)
+  return array
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
   array = array.push()
+  return array
 }
