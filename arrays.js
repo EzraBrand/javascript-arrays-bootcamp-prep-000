e986a7c773e0b89a0d59a0faa7c40d948fe25f5a
Ezra Brand <ezrabrand@gmail.com>
2019-04-03 17:10:00 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,4 +1,4 @@
-var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittle']
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 addElementToBeginningOfArray(x) {
   let myArray = []
